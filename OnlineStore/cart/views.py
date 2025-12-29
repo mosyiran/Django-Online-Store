5bc1a2fad4f7d944bc534f5245bf816bd6bb952c
mosyiran <eng.goudarzi@outlook.com>
2025-12-29 15:39:48 +0330

    add product to session

--- OnlineStore/cart/views.py
+++ OnlineStore/cart/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from http.client import responses
+from django.shortcuts import render, get_object_or_404
+from shop.views import product
+from .cart import Cart
+from shop.models import Product
+from django.http import JsonResponse
 
 def cart_summary(request):
     return render(request, 'cart_summary.html', {})
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+        product_id = int( request.POST.get('product_id'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product)
+        response = JsonResponse({'product_name' : product.name})
+        return response
+
 
 def cart_update(request):
     pass
