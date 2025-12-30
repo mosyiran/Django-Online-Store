a17ec25261cd7b8d3f7cc997d6fe7ee764e1840f
mosyiran <eng.goudarzi@outlook.com>
2025-12-30 15:11:16 +0330

    shopping bascket sync with session

--- OnlineStore/cart/views.py
+++ OnlineStore/cart/views.py
@@ -15,7 +15,10 @@ def cart_add(request):
         product_id = int( request.POST.get('product_id'))
         product = get_object_or_404(Product, id=product_id)
         cart.add(product=product)
-        response = JsonResponse({'product_name' : product.name})
+
+        cart_quantity = cart.__len__()
+        # response = JsonResponse({'product_name' : product.name})
+        response = JsonResponse({'qty' : cart_quantity})
         return response
 
 
