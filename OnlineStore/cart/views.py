90797b02e9fe03cdc863f9ed3568a1a015a41dd4
mosyiran <eng.goudarzi@outlook.com>
2026-01-01 12:03:08 +0330

    Edit count in shopping basket added

--- OnlineStore/cart/views.py
+++ OnlineStore/cart/views.py
@@ -18,7 +18,6 @@ def cart_summary(request):
     )
 
 
-
 def cart_add(request):
     cart = Cart(request)
 
@@ -26,7 +25,7 @@ def cart_add(request):
         product_id = int(request.POST.get('product_id'))
         product_qty = int(request.POST.get('product_qty'))
         product = get_object_or_404(Product, id=product_id)
-        cart.add(product=product, quantity = product_qty)
+        cart.add(product=product, quantity=product_qty)
 
         cart_quantity = cart.__len__()
         # response = JsonResponse({'product_name' : product.name})
@@ -35,7 +34,14 @@ def cart_add(request):
 
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        cart.update(product=product_id, quantity=product_qty)
+        response = JsonResponse({'qty': product_qty})
+        return response
 
 
 def cart_delete(request):
