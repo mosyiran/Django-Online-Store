41d0d55dc3e2d2eb0a9cf1bd2dcf97fa023bde37
mosyiran <eng.goudarzi@outlook.com>
2026-01-25 15:53:08 +0330

    Edit Profile User in Admin Page

--- OnlineStore/shop/admin.py
+++ OnlineStore/shop/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
 from . import models
 
 admin.site.register(models.Category)
 admin.site.register(models.Customer)
 admin.site.register(models.Product)
 admin.site.register(models.Order)
+admin.site.register(models.Profile)
+
+
+class ProfileInline(admin.StackedInline):
+    model = models.Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- OnlineStore/shop/models.py
+++ OnlineStore/shop/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 import datetime
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
+
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=100)
@@ -13,14 +18,40 @@ class Customer(models.Model):
     phone = models.CharField(max_length=20)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=50)
+
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateField(User, auto_now=True)
+    phone = models.CharField(max_length=20, blank=True)
+    address1 = models.CharField(max_length=100, blank=True)
+    address2 = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=100, blank=True)
+    zipcode = models.CharField(max_length=100, blank=True)
+    country = models.CharField(max_length=100, default='Iran')
+
+    def __str__(self):
+        return self.user.username
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+
+post_save.connect(create_profile, sender=User)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100, default='', blank=True, null=True)
     price = models.DecimalField(default=0, max_digits=12, decimal_places=0)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default= 1)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     picture = models.ImageField(upload_to='upload/products/')
     # SIZES = (
     #     ('s', 32),
@@ -31,9 +62,11 @@ class Product(models.Model):
     # size = models.CharField(max_length=4, choices=SIZES, default=32)
     is_sale = models.BooleanField(default=False)
     sale_price = models.DecimalField(default=0, max_digits=12, decimal_places=0)
-    star = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)] )
+    star = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
+
     def __str__(self):
         return self.name
+
 
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -43,6 +76,6 @@ class Order(models.Model):
     phone = models.CharField(max_length=20, blank=True)
     date = models.DateTimeField(default=datetime.datetime.today())
     status = models.BooleanField(default=False)
+
     def __str__(self):
         return self.product
-
